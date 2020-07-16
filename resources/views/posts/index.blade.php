a340502e626abe69f7e3543ba8ddb12a92366d98
subkt2013 <subkt2013@gmail.com>
2020-07-17 00:11:55 +0900

    レイアウト変更の途中

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,27 +1,31 @@
 @extends('Front.layouts')
 
 @section('content')
-<div class="container">
-<div class="mb-4">
+    <div class="mb-4">
+    タグ <br>
             <a href="{{ route('posts.create') }}" class="btn btn-primary">
                 掲示板に投稿する
             </a>
-        </div>
-        @foreach ($posts as $post)
-            <div class="card ">
-                <a class="card-body" href="{{ route('posts.show', ['post' => $post]) }}">
-                    {{$post->title}}
-                </a>
-                <div class="card-footer">
-                    投稿日時{{$post->created_at}}
-                    コメント数{{$post->comments->count()}}
+    </div>
+        
+            <div class="f-container">
+            @foreach ($posts as $post)
+                <div class="f-item">
+                    <div class="card mb-4">
+                        <div class="card-header">投稿のタイトル</div>
+                        <a class="card-body" href="{{ route('posts.show', ['post' => $post]) }}">
+                            {{$post->title}}
+                        </a>
+                        <div class="card-footer">
+                            投稿日時{{$post->created_at}}
+                            コメント数{{$post->comments->count()}}
+                        </div>
+                    </div>
                 </div>
+            @endforeach
             </div>
-        @endforeach
-
             <div class="d-flex justify-content-center mb-5">
                 {{ $posts->links() }}
             </div>
-</div>
 
 @endsection('content')
