26d88a617ca5d207c746c092d187d887ba6d4c4c
subkt2013 <49891514+subkt2013@users.noreply.github.com>
2021-11-22 16:33:09 +0900

    コメント機能の削除

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -19,37 +19,7 @@
                     {!! nl2br(e($post->body)) !!}
                 </p>
             </div>
-      
-        <form class="mb-4" method="POST" action="{{ route('comments.store') }}">
-            @csrf
-            <input
-            name="post_id"
-            type="hidden"
-            value="{{ $post->id }}"
-            >
-
-            <div class="form-group">
-                <label for="body">
-                コメント
-                </label>
-
-                <textarea
-                id="body"
-                name="body"
-                class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"
-                rows="4"
-                >{{ old('body') }}</textarea>
-                @if ($errors->has('body'))
-                <div class="invalid-feedback">
-                {{ $errors->first('body') }}
-                </div>
-                @endif
-            </div>
-
             <div class="mt-4">
-                <button type="submit" class="btn btn-primary">
-                コメントする
-                </button>
                 <a class="btn btn-secondary" href="{{ route('top') }}">
                 戻る
                 </a>
