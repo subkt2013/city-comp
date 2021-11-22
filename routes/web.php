6ecfda733beb7258b4eef90ab8c8764d79bf0bbf
subkt2013 <49891514+subkt2013@users.noreply.github.com>
2021-11-22 16:39:51 +0900

    いらないapiの削除

--- routes/web.php
+++ routes/web.php
@@ -27,37 +27,6 @@ Route::group(['prefix' => '/posts'],function(){
 });
 Route::resource('comments', 'CommentsController', ['only' => ['store']]);
 
-//nbaについてのルート
-Route::group(['prefix' => '/nba'],function(){
-    Route::get('/','NbaPostsController@index')->name('posts.nba.index');
-    Route::get('/create', 'NbaPostsController@create')->name('posts.nba.create');
-    Route::post('/', 'NbaPostsController@store')->name('posts.nba.store');
-    Route::get('/{post}', 'NbaPostsController@show')->name('posts.nba.show');
-    Route::get('/{post}/edit', 'NbaPostsController@edit')->name('posts.nba.edit');
-    Route::put('/{post}', 'NbaPostsController@update')->name('posts.nba.update');
-    Route::delete('/{post}', 'NbaPostsController@delete')->name('posts.nba.delete');
-});
-//書き方をpostに合わせる nbapostのように１ずつ定義する
-//Route::resource('nba_comments', 'NbaCommentsController', ['only' => ['store']])->name('comments.nba.store');
-//Route::post('nba_comments', 'NbaCommentsController@store')->name('comments.nba.store');
-Route::resource('nba_comments', 'NbaCommentsController', ['only' => ['store']]);
-
-//バスケ仲間についてのルート
-Route::group(['prefix' => '/with'],function(){
-    Route::get('/','WithPostsController@index')->name('posts.with.index');
-    Route::get('/create', 'WithPostsController@create')->name('posts.with.create');
-    Route::post('/', 'WithPostsController@store')->name('posts.with.store');
-    Route::get('/{post}', 'WithPostsController@show')->name('posts.with.show');
-    Route::get('/{post}/edit', 'WithPostsController@edit')->name('posts.with.edit');
-    Route::put('/{post}', 'WithPostsController@update')->name('posts.with.update');
-    Route::delete('/{post}', 'WithPostsController@delete')->name('posts.with.delete');
-});
-
-//書き方をpostに合わせる nbapostのように１ずつ定義する
-//Route::resource('nba_comments', 'NbaCommentsController', ['only' => ['store']])->name('comments.nba.store');
-//Route::post('nba_comments', 'NbaCommentsController@store')->name('comments.nba.store');
-Route::resource('with_comments', 'WithCommentsController', ['only' => ['store']]);
-
 //認証
 Auth::routes();
 
