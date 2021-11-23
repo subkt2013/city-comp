3dadf01d1a4772b819a9f9f73f30b9967672d503
subkt2013 <49891514+subkt2013@users.noreply.github.com>
2021-11-24 02:00:42 +0900

    規約の場所変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,28 +3,31 @@
 @section('content')
     <div class="mb-4">
         <br>
-            <a href="{{ route('posts.create') }}" class="btn btn-primary">
-                掲示板に投稿する
-            </a>
+        <a href="{{ route('posts.create') }}" class="btn btn-primary">
+            掲示板に投稿する
+        </a>
     </div>
-            <div class="row">
-            @foreach ($posts as $post)
-                <div class="col-sm-4">
-                    <div class="card">
-                        <div class="card-header"><a href="{{ route('posts.show', ['post' => $post]) }}"> {{$post->title}}</a></div>
-                        <a class="card-body">
-                            {{$post->body}}
-                        </a>
-                        <div class="card-footer">
-                            性別: {{$post->gender}} <br>
-                            投稿日時: {{$post->created_at}}
-                        </div>
+    <div class="row">
+        @foreach ($posts as $post)
+            <div class="col-sm-4">
+                <div class="card">
+                    <div class="card-header"><a href="{{ route('posts.show', ['post' => $post]) }}"> {{$post->title}}</a></div>
+                    <a class="card-body">
+                        {{$post->body}}
+                    </a>
+                    <div class="card-footer">
+                        性別: {{$post->gender}} <br>
+                        投稿日時: {{$post->created_at}}
                     </div>
                 </div>
-            @endforeach
             </div>
-            <div class="d-flex justify-content-center mb-5">
-                {{ $posts->links() }}
-            </div>
+        @endforeach
+    </div>
+    <div class="d-flex justify-content-center mb-5">
+        {{ $posts->links() }}
+    </div>
+    <footer>
+        <div class="container"><a href="{{ route('terms') }}">規約</a> </div>
+    </footer>
 
 @endsection('content')
