36aec9dd47c9578aab8ad6f0baeb6e853bba8af2
subkt2013 <49891514+subkt2013@users.noreply.github.com>
2022-06-05 00:31:42 +0900

    kakao外す

--- resources/views/Front/layouts.blade.php
+++ resources/views/Front/layouts.blade.php
@@ -16,7 +16,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <meta name="description" content="zoom仲間やskype仲間やcacao仲間や飲み友だちを探すサイトです。">
+    <meta name="description" content="zoom仲間やskype仲間や飲み友だちを探すサイトです。">
     <title>zoom掲示板</title>
     <link
         rel="stylesheet"
@@ -35,7 +35,7 @@
           <div class="card">
             <div class="card-body">
               <h5 class="card-title"><a href="{{route('top')}}">zoom掲示板</a></h5>
-              <p class="card-text">zoom仲間やskype仲間やcacao仲間や飲み友だちを探すサイトです。お問い合わせは<a href="https://twitter.com/zoom20021449">zoom掲示板Twitterアカウント</a> へ</div>
+              <p class="card-text">zoom仲間やskype仲間や飲み友だちを探すサイトです。お問い合わせは<a href="https://twitter.com/zoom20021449">zoom掲示板Twitterアカウント</a> へ</div>
           </div>
         </div>
       </div>
@@ -46,5 +46,19 @@
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-</body>
+  <script>
+        function copyToClipboard() {
+            // コピー対象をJavaScript上で変数として定義する
+            var copyTarget = document.getElementById("copyTarget");
+
+            // コピー対象のテキストを選択する
+            copyTarget.select();
+
+            // 選択しているテキストをクリップボードにコピーする
+            document.execCommand("Copy");
+
+            // コピーをお知らせする
+            alert("コピーできました！ : " + copyTarget.value);
+        }
+    </script></body>
 </html>
